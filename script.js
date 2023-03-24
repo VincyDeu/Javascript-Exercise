853372e10ef0e5d56a1ffd3563afd1472565e631
VincyDeu <126015477+VincyDeu@users.noreply.github.com>
2023-03-24 20:06:58 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+let y;
+y = 12 + 20;
+console.log(y);
+
+let x = 12;
+console.log(x);
+
+let name = "Vincy";
+console.log(name);
+
+z = x-4;
+console.log(z);
+
+let name1 = "john";
+let name2 = "John";
+
+verifica1 = console.log(name1 === name2);
+verifica2 = console.log(name1 !== name2);
+
+verifica1 = name1===name2.toLowerCase();
+console.log(verifica1);
+
